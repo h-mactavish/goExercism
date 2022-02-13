9ccf790c355a0a3af5221c57d9e4f16bb338bdc7
Himanshu MacTavish <hmactavish76@hotmail.com>
2022-02-13 11:50:21 +0530

    Coding practice for exercsim in GoLang

---
+++ cars-assemble/.history/cars_assemble_20220213114340.go
@@ -0,0 +1,22 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * (successRate / 100.0)
+	//panic("CalculateWorkingCarsPerHour not implemented")
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return productionRate / 60 * int(successRate/100.0)
+	//panic("CalculateWorkingCarsPerMinute not implemented")
+}
+
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	result := carsCount/10*95000 + carsCount%10*10000
+	return unit(result)
+	panic("CalculateCost not implemented")
+}
---
+++ cars-assemble/cars_assemble.go
@@ -0,0 +1,23 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * (successRate / 100.0)
+	//panic("CalculateWorkingCarsPerHour not implemented")
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	result := float64(productionRate/60) * (successRate / 100.0)
+	return int(result)
+	//panic("CalculateWorkingCarsPerMinute not implemented")
+}
+
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	result := carsCount/10*95000 + carsCount%10*10000
+	return uint(result)
+	//panic("CalculateCost not implemented")
+}
