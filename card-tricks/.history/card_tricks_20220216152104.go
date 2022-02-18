32d9570f9da34cd88abcac5d36902726098499d7
Himanshu MacTavish <hmactavish76@hotmail.com>
2022-02-18 08:48:46 +0530

    Did Exercise card tricks

---
+++ card-tricks/.history/card_tricks_20220216152104.go
@@ -0,0 +1,33 @@
+package cards
+
+// GetItem retrieves an item from a slice at given position. The second return value indicates whether
+// the given index exists in the slice or not.
+func GetItem(slice []int, index int) (int, bool) {
+	if index <= 0 || index > len(slice) {
+		return 0, false
+	} else {
+		return slice[index], true
+	}
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if index > len(slice) {
+		slice = append(slice, value)
+	} else {
+		slice[index] = value
+	}
+
+	//panic("Please implement the SetItem function")
+}
+
+// PrefilledSlice creates a slice of given length and prefills it with the given value.
+func PrefilledSlice(value, length int) []int {
+	panic("Please implement the PrefilledSlice function")
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	panic("Please implement the RemoveItem function")
+}
